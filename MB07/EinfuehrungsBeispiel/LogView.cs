937f11b47a9f2293ff90628904f263969535f1ad
Larissa Fitze <laerryies@gmail.com>
2023-07-20 08:26:58 +0200

    Add Einführungsbeispiel

---
+++ MB07/EinfuehrungsBeispiel/LogView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB07.EinfuehrungsBeispiel
+{
+    public static class LogView
+    {
+
+        public static void PrintReadAllLogs()
+        {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            Console.WriteLine("\nStart ReadAllLogs");
+            var lineCount = ReadAllLogs();
+            Console.WriteLine("Number of lines: " + lineCount);
+            Console.WriteLine("Time elapsed: {0:0.0000}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+            // *** List<> in CountUniqueIPs
+            stopWatch.Restart();
+            Console.WriteLine("\nStart CountUniqueIPs with List");
+            var ipCount = CountUniqueIPs(new List<string>());
+            Console.WriteLine("Number of unique IPs: " + ipCount);
+            Console.WriteLine("Time elapsed: {0:0.0000}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+            // *** HashSet<> in CountUniqueIPs
+            stopWatch.Restart();
+            Console.WriteLine("\nStart CountUniqueIPs with HashSet");
+            var ipCount2 = CountUniqueIPs(new HashSet<string>());
+            Console.WriteLine("Number of unique IPs: " + ipCount2);
+            Console.WriteLine("Time elapsed: {0:0.0000}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+            Console.ReadLine();
+        }
+
+        private static int ReadAllLogs()
+        {
+            var logReader = new LogReader();
+            var linesSeen = 0;
+            foreach (var line in logReader)
+            {
+                var ip = line.GetIP();
+                linesSeen++;
+            }
+            return linesSeen;
+        }
+
+
+
+        private static int CountUniqueIPs(ICollection<string> ipsSeen)
+        {
+            var logReader = new LogReader();
+            foreach (var logLine in logReader)
+            {
+                var ip = logLine.GetIP();
+                if (!ipsSeen.Contains(ip))
+                    ipsSeen.Add(ip);
+            }
+            return ipsSeen.Count;
+        }
+    }
+}
--- MB07/Program.cs
+++ MB07/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MB07.EinfuehrungsBeispiel;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Einführungsbeispiel
+        LogView.PrintReadAllLogs();
+        Console.ReadLine();
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
