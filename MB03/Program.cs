7c704d0b5689a29667a4cf8eaa0b71e4be3d2c79
Larissa Fitze <laerryies@gmail.com>
2024-07-25 20:53:44 +0200

    Remove

--- MB03/Program.cs
+++ MB03/Program.cs
@@ -14,9 +14,7 @@ namespace MB03
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             
-            Application.Run(new A4_1_4());
-
-            //Application.Run(new A8_2_1());
+            Application.Run(new A8_2_1());
 
         }
     }
